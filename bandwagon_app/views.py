29b675190b585099ae4628fc3a006ad55d8e5148
carsonbutterfield <carsonbutterfield98@gmail.com>
2019-11-06 14:13:15 -0800

    staging for merge

--- bandwagon_app/views.py
+++ bandwagon_app/views.py
@@ -4,6 +4,6 @@ from django.core import serializers
 from .models import Artist, Band
 # Create your views here.
 def band_detail(req,pk):
-    band = "Band.objects.get(id=pk)"
+    band = Band.objects.get(id=pk)
     context = {"band":band}
     return render(req,'band_detail.html',context)
