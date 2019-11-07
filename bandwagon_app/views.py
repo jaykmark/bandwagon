7e4c79be0727e581e6d0b75ee885ff0a89e6c6e7
carsonbutterfield <carsonbutterfield98@gmail.com>
2019-11-07 15:25:28 -0800

    one user, one artist

--- bandwagon_app/views.py
+++ bandwagon_app/views.py
@@ -18,50 +18,50 @@ def band_detail(req,pk):
     return render(req,'band_detail.html',context)
 
 
-def artist_detail(request,pk):
+def artist_detail(req,pk):
     artist = Artist.objects.get(id=pk)
     context = {"artist":artist}
-    return render(request, 'artist_detail.html', context)
+    return render(req, 'artist_detail.html', context)
 
 # -------- LISTS ---------- #
 
-def band_list(request):
+def band_list(req):
     bands = Band.artist.all()
     context = {"bands":bands}
-    return render(request, 'band_list.html', context)
+    return render(req, 'band_list.html', context)
 
 # Create
 @login_required
-def band_create(request):
-    if request.method == 'POST':
-        form = BandForm(request.POST)
+def band_create(req):
+    if req.method == 'POST':
+        form = BandForm(req.POST)
         if form.is_valid():
             band = form.save(commit = False)
-            band.owner = Artist.objects.get(user = request.user)
+            band.owner = Artist.objects.get(user = req.user)
             band.save()
             return redirect('band_list.html', pk=band.pk)
     else:
         form = BandForm()
         context = {'form':form, 'header':"Add New Band"}
-        return render(request, 'band_form.html', context)
+        return render(req, 'band_form.html', context)
 
 # Update
 
-def band_edit(request, pk, band_pk):
+def band_edit(req, pk, band_pk):
     band = Band.objects.get(id=band_pk)
-    if request.method == 'POST':
-        form = BandForm(request.POST, instance=band)
+    if req.method == 'POST':
+        form = BandForm(req.POST, instance=band)
         if form.is_valid():
             band = form.save()
             return redirect('band_detail.html', pk=band.pk)
     else:
         form = BandForm(instance=band)
         context = {'form':form, 'header':f"Edit {band.name}"}
-        return render(request, 'band_form.html', context)
+        return render(req, 'band_form.html', context)
 
 # Delete
 
-def band_delete(request, pk, band_pk):
+def band_delete(req, pk, band_pk):
     Band.objects.get(id=band_pk).delete()
     return redirect('band_list', pk=pk)
 
@@ -69,51 +69,56 @@ def band_delete(request, pk, band_pk):
 
 # Detail
 
-def artist_detail(request,pk):
+def artist_detail(req,pk):
     artist = Artist.objects.get(id=pk)
     context = {"artist":artist}
-    return render(request, 'artist_detail.html', context)
+    return render(req, 'artist_detail.html', context)
 
 # List
 
-def artist_list(request):
+def artist_list(req):
     artists = Artist.objects.all()
     context = {"artists":artists}
-    return render(request, 'artist_list.html', context)
+    return render(req, 'artist_list.html', context)
 
 # Create
 
-def artist_create(request):
-    if request.method == 'POST':
-        form = ArtistForm(request.POST)
+def artist_create(req):
+    artist = Artist.objects.get(id=req.user.id)
+    if req.method == 'POST':
+        form = ArtistForm(req.POST,instance = artist)
         if form.is_valid():
             artist = form.save(commit = False)
-            artist.user = request.user
+            artist.user = req.user
             artist.save()
             return redirect('artist_detail', pk=artist.pk)
     else:
-        form = ArtistForm()
+       
+        if artist is not None:
+            form = ArtistForm(instance = artist)
+        else:
+            form = ArtistForm()
         context = {'form':form, 'header':"Add New Artist"}
-        return render(request, 'artist_form.html', context)
+        return render(req, 'artist_form.html', context)
 
 # Update
 
 
-def artist_edit(request, pk):
+def artist_edit(req, pk):
     artist = Artist.objects.get(id=pk)
-    if request.method == 'POST':
-        form = ArtistForm(request.POST, instance=artist)
+    if req.method == 'POST':
+        form = ArtistForm(req.POST, instance=artist)
         if form.is_valid():
             artist = form.save()
             return redirect('artist_detail', pk=artist.pk)
     else:
         form = ArtistForm(instance=artist)
         context = {'form':form, 'header':f"Edit {artist.stage_name}"}
-        return render(request, 'artist_form.html', context)
+        return render(req, 'artist_form.html', context)
 
 # Delete
 
-def artist_delete(request, pk):
+def artist_delete(req, pk):
     Artist.objects.get(id=pk).delete()
     return redirect('artist_list')
 
@@ -134,3 +139,9 @@ def decline_invite(req,invite_pk):
      band_id = invite.band.id
      invite.delete()
      return redirect('band_detail',pk=band_id)
+
+@login_required
+def createInvite(req,band_pk,sender):
+
+# Sender Property: True = Artist; False = Band
+  
