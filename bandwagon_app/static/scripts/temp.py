ea3a01f50c689690ecd3596c7444e735fe79c96f
carsonbutterfield <carsonbutterfield98@gmail.com>
2019-11-09 13:01:16 -0800

    removed temp file

--- bandwagon_app/static/scripts/temp.py
+++
@@ -1,7 +0,0 @@
-def band_detail(req,pk):
-    band = Band.objects.get(id=pk)
-    invites =  Invite.objects.filter(band=pk)
-    members = BandMember.objects.filter(band=pk)
-    invites = filter(lambda invite: invite.sender != True,invites)
-    context = {"band":band,"invites":invites,"members":members}
-    return render(req,'band_detail.html',context)
